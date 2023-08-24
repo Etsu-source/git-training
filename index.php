1574fca70899182836e8978dfac9850a2bb1fd00
Jose Bone <joserock7395@gmail.com>
2023-08-24 13:47:10 -0500

    actualizando con merge continuar con el desarrollo

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+<<<<<<< HEAD
 echo 'Hola bb uwu, can u learn it? aqui estabe el "yeahhh" veamos que pasa si se recupera';
+=======
+echo 'Hola bb uwu, can u learn it? yeahhh';
+>>>>>>> 57fefcc (actualizando con merge)
 
 ?>
